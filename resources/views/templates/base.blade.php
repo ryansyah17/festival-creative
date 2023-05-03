dcafe5c0592a5394f72d4c38d89de294bb9eba25
ryansyah17 <ryansyahputra553@gmail.com>
2023-05-03 19:23:59 +0700

    fix bug base

--- resources/views/templates/base.blade.php
+++ resources/views/templates/base.blade.php
@@ -34,24 +34,20 @@
 
 </head>
 
-<body class="index-page sidebar-collapse" data-background-color="black">
+<body class="index-page sidebar-collapse">
     <nav class="navbar navbar-expand-lg bg-primary fixed-top navbar-transparent " color-on-scroll="400">
-        <div class="container animate__animated animate__fadeInDown">
+        <div class="container">
             <div class="navbar-translate">
-                <a class="navbar-brand" href="#" onclick="scrollToHome()" rel="tooltip"
-                    title="Bukittinggi Creative Week Festival" data-placement="bottom">
+                <a class="navbar-brand" href="#" onclick="scrollToHome()" rel="tooltip" title="Bukittinggi Creative Week Festival" data-placement="bottom">
                     BCWF 2023
                 </a>
-                <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navigation" aria-controls="navigation-index" aria-expanded="false"
-                    aria-label="Toggle navigation">
+                <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-bar top-bar"></span>
                     <span class="navbar-toggler-bar middle-bar"></span>
                     <span class="navbar-toggler-bar bottom-bar"></span>
                 </button>
             </div>
-            <div class="collapse navbar-collapse justify-content-end" id="navigation"
-                data-nav-image="./assets/img/blurred-image-1.jpg">
+            <div class="collapse navbar-collapse justify-content-end" id="navigation" data-nav-image="./assets/img/blurred-image-1.jpg">
                 <ul class="navbar-nav">
                     <li class="nav-item">
                         <a class="nav-link" href="javascript:void(0)" onclick="scrollToHome()">
@@ -84,22 +80,19 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" rel="tooltip" title="Follow us on Twitter" data-placement="bottom"
-                            href="https://twitter.com/bcwf2023" target="_blank">
+                        <a class="nav-link" rel="tooltip" title="Follow us on Twitter" data-placement="bottom" href="https://twitter.com/bcwf2023" target="_blank">
                             <i class="fab fa-twitter"></i>
                             <p class="d-lg-none d-xl-none">Twitter</p>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" rel="tooltip" title="Like us on Facebook" data-placement="bottom"
-                            href="https://www.facebook.com/bcwf2023" target="_blank">
+                        <a class="nav-link" rel="tooltip" title="Like us on Facebook" data-placement="bottom" href="https://www.facebook.com/bcwf2023" target="_blank">
                             <i class="fab fa-facebook-square"></i>
                             <p class="d-lg-none d-xl-none">Facebook</p>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" rel="tooltip" title="Follow us on Instagram" data-placement="bottom"
-                            href="https://www.instagram.com/bcwf2023" target="_blank">
+                        <a class="nav-link" rel="tooltip" title="Follow us on Instagram" data-placement="bottom" href="https://www.instagram.com/bcwf2023" target="_blank">
                             <i class="fab fa-instagram"></i>
                             <p class="d-lg-none d-xl-none">Instagram</p>
                         </a>
